daed3c2c404cec852658d6be77166df581266df2
rakib <rakibcse201@gmail.com>
2025-06-28 11:47:51 +0600

    add db

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-URL_DATABASE = 'postgresql://postgres:root@localhost:5432/QuizApplicationYT'
-
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", 
+    "postgresql://postgres:root@localhost:5432/QuizApplicationYT"
+)
 engin = create_engine(URL_DATABASE)
 sessionlocal = sessionmaker(autoflush=False, autocommit = False, bind=engin)
 Base = declarative_base()
